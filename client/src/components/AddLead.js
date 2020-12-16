31ef8075d94986d141f2c02e9cd81e344b97499f
PANKAJ SHARMA <gaur.ps21@outlook.com>
2020-12-17 00:35:25 +0530

    worked on lead

--- client/src/components/AddLead.js
+++ client/src/components/AddLead.js
@@ -1,43 +1,72 @@
 import React, { Component } from 'react';
+import uuid from 'react-uuid';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 
 
 class AddLead extends Component {
 
   state = {
-    lead_id: null,
-    lead_name: "",
-    company_name: "",
-    position: "",
-    tel: null,
+    lead_id: uuid(),
+    created_on: "",
+    source: "",
+    assigned_to: "",
+    client_name: "",
     email: "",
-    last_contacted_on: "",
+    tel_no: "",
+    position: "",
+    company: "",
+    domain: "",
+    status: false,
+    confidence: "",
+    comments: "",
+    connected_times: "",
+    is_delete: false,
   }
 
-  addLead = () => {
+  addLead = (e) => {
     const lead = {
       lead_id: this.state.lead_id,
-      lead_name: this.state.lead_name,
-      company_name: this.state.company_name,
-      position: this.state.position,
-      tel: this.state.tel,
+      created_on: this.state.created_on,
+      source: this.state.source,
+      assigned_to: this.state.assigned_to,
+      client_name: this.state.client_name,
       email: this.state.email,
-      last_contacted_on: this.state.last_contacted_on,
+      tel_no: this.state.tel_no,
+      position: this.state.position,
+      company: this.state.company,
+      domain: this.state.domain,
+      status: this.state.status,
+      confidence: this.state.confidence,
+      comments: this.state.comments,
+      connected_times: this.state.connected_times,
+      is_delete: this.state.is_delete,
     }
 
     if(Object.keys(lead).length === 0 && lead.constructor === Object){
       axios.post('/api/leads', lead)
         .then(res => {
           if(res.data){
+            toast.success("added successfully");
+
             this.props.getLeads();
             this.setState({
-              lead_id: null,
-              lead_name: "",
-              company_name: "",
-              position: "",
-              tel: null,
+               lead_id: uuid(),
+              created_on: "",
+              source: "",
+              assigned_to: "",
+              client_name: "",
               email: "",
-              last_contacted_on: ""
+              tel_no: "",
+              position: "",
+              company: "",
+              domain: "",
+              status: false,
+              confidence: "",
+              comments: "",
+              connected_times: "",
+              is_delete: false,
             })
           }
         })
@@ -47,40 +76,210 @@ class AddLead extends Component {
     }
   }
 
+  updateLead = (e) => {
+
+    const lead = {
+      lead_id: uuid(),
+      created_on: "",
+      source: "",
+      assigned_to: "",
+      client_name: "",
+      email: "",
+      tel_no: "",
+      position: "",
+      company: "",
+      domain: "",
+      status: false,
+      confidence: "",
+      comments: "",
+      connected_times: "",
+      is_delete: false,
+    }
+
+    if (Object.keys(lead).length !== 0 && lead.constructor === Object) {
+      var id = this.state._id;
+      axios.put(`/api/clients/${id}`, lead)
+        .then(res => {
+          if (res.data) {
+            this.props.isButtonEdit(false);
+            this.showAlert("success", "Lead updated!");
+
+            this.props.getLeads();
+            this.setState({
+              _id: "",
+              lead_id: uuid(),
+              created_on: "",
+              Source: "",
+              assigned_to: "",
+              client_name: "",
+              email: "",
+              tel_no: "",
+              position: "",
+              company: "",
+              domain: "",
+              status: false,
+              confidence: "",
+              comments: "",
+              connected_times: "",
+              is_delete: false,
+            })
+          }
+        })
+        .catch(err => console.log(err))
+    } else {
+      console.log('input field required')
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
   handleChange = (e) => {
     this.setState({
-      lead_id: null,
-      lead_name: "",
-      company_name: "",
-      position: "",
-      tel: null,
-      email: "",
-      last_contacted_on: ""
+      [e.target.name]: e.target.value
     })
+  }
+
+
+  componentDidUpdate(prevProp) {
+    if (prevProp.lead.lead_id !== this.props.lead.lead_id) {
+      let {
+        lead
+      } = this.props;
+      this.setState({
+        lead_id: lead.lead_id,
+        created_on: lead.created_on,
+        source: lead.source,
+        assigned_to: lead.assigned_to,
+        client_name: lead.client_name,
+        email: lead.email,
+        tel_no: lead.tel_no,
+        position: lead.position,
+        company: lead.company,
+        domain: lead.domain,
+        status: lead.status,
+        confidence: lead.confidence,
+        comments: lead.comments,
+        connected_times: lead.connected_times,
+        is_delete: lead.is_delete,
+      });
+    }
   }
 
   render() {
     let { 
+      _id,
       lead_id,
-      lead_name,
-      company_name,
-      position,
-      tel,
+      created_on,
+      source,
+      assigned_to,
+      client_name,
       email,
-      last_contacted_on 
+      tel_no,
+      position,
+      company,
+      domain,
+      status,
+      confidence,
+      comments,
+      connected_times,
+      is_delete,
+      color,
+      visible,
+      msg
     } = this.state;
 
     return (
       <div>
-        <input type="hidden" value={lead_id} />
-        <input type="text" onChange={this.handleChange} value={lead_name} />
-        <input type="text" onChange={this.handleChange} value={company_name} />
-        <input type="text" onChange={this.handleChange} value={position} />
-        <input type="text" onChange={this.handleChange} value={tel} />
-        <input type="text" onChange={this.handleChange} value={email} />
-        <input type="text" onChange={this.handleChange} value={last_contacted_on} />
-        <button onClick={this.addLead}>add Lead</button>
-      </div>
+      <form>
+        <div className="form-group">
+          <input type="text" name="lead_id" placeholder="Lead ID"
+            className="form-control" onChange={this.handleChange}
+            value={lead_id} required />
+        </div>
+        <div className="form-group">
+          <input type="date" name="created_on" placeholder="Created on"
+            className="form-control" onChange={this.handleChange}
+            value={this.props.formatDate(created_on)} required />
+        </div>
+        <div className="form-group">
+          <input type="text" name="source" placeholder="Source"
+            className="form-control" onChange={this.handleChange}
+            value={source} required />
+        </div>
+        <div className="form-group">
+          <input type="text" name="assigned_to" placeholder="Assigned to"
+            className="form-control" onChange={this.handleChange}
+            value={assigned_to} required />
+        </div>
+        <div className="form-group">
+          <input type="text" name="client_name"
+            placeholder="Client name" className="form-control"
+            onChange={this.handleChange} value={client_name} required />
+        </div>
+        <div className="form-group">
+          <input type="text" name="company"
+            placeholder="Company" className="form-control"
+            onChange={this.handleChange}
+            value={company} required />
+        </div>
+        <div className="form-group">
+          <input type="text" name="position" placeholder="Position"
+            className="form-control" onChange={this.handleChange}
+            value={position} required />
+        </div>
+        <div className="form-group">
+          <input type="text" name="tel_no" placeholder="Phone no."
+            className="form-control" onChange={this.handleChange}
+            value={tel_no} required />
+        </div>
+        <div className="form-group">
+          <input type="email" name="email" placeholder="Email ID"
+            className="form-control" onChange={this.handleChange}
+            value={email} required />
+        </div>
+        <div className="form-group">
+          <input type="text" name="domain" placeholder="Domain"
+            className="form-control" onChange={this.handleChange}
+            value={domain} required />
+        </div>
+        <div className="form-group">
+          <input type="text" name="status" placeholder="status"
+            className="form-control" onChange={this.handleChange}
+            value={status} required />
+        </div>
+        <div className="form-group">
+          <input type="text" name="confidence" placeholder="Confindence"
+            className="form-control" onChange={this.handleChange}
+            value={confidence} required />
+        </div>
+        <div className="form-group">
+          <input type="text" name="connected_times" placeholder="Connected Times?"
+            className="form-control" onChange={this.handleChange}
+            value={connected_times} required />
+        </div>
+        <div className="form-group">
+          <input type="text" name="is_delete" placeholder="Deleted?"
+            className="form-control" onChange={this.handleChange}
+            value={is_delete} required />
+        </div>
+        <div className="form-group">
+          <textarea type="text" name="comments" placeholder="Comments"
+            className="form-control" onChange={this.handleChange}
+            value={comments} required></textarea>
+        </div>
+        <input type="hidden" name="_id" value={_id} />
+        {this.props.isEdit ? <button type="button" className="btn btn-success"
+          onClick={this.updateLead}> Update Lead </button>
+          : <button type="button" className="btn btn-success"
+            onClick={this.addLead}> Add Lead </button>
+        }
+      </form>
+      <>
+    <ToastContainer autoClose={false} />
+    </>
+    </div>
     )
   }
 }
--- client/src/components/Lead.js
+++ client/src/components/Lead.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import uuid from 'react-uuid';
 
 import AddLead from './AddLead';
 import ListLead from './ListLead';
@@ -7,17 +8,36 @@ import ListLead from './ListLead';
 class Lead extends Component {
 
   state = {
-    leads: []
+    leads: [],
+    lead: {
+      _id: "",
+      lead_id: uuid(),
+      created_on: "",
+      source: "",
+      assigned_to: "",
+      client_name: "",
+      email: "",
+      tel_no: "",
+      position: "",
+      company: "",
+      domain: "",
+      status: false,
+      confidence: "",
+      comments: [],
+      connected_times: "",
+      is_delete: false,
+    },
+    isEdit: false,
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getLeads();
   }
 
   getLeads = () => {
     axios.get('/api/leads')
       .then(res => {
-        if(res.data){
+        if (res.data) {
           this.setState({
             leads: res.data
           })
@@ -30,21 +50,80 @@ class Lead extends Component {
 
     axios.delete(`/api/leads/${id}`)
       .then(res => {
-        if(res.data){
+        if (res.data) {
           this.getLeads()
         }
       })
       .catch(err => console.log(err))
   }
 
-  render() {
-    let { leads } = this.state;
+  getLead = (id) => {
+    axios.get(`/api/leads/${id}`)
+      .then(res => {
+        if (res.data) {
+          this.setState({
+            lead: res.data
+          });
+        }
+      })
+  }
 
-    return(
+  editLead = (id) => {
+    this.setState({
+      isEdit: true
+    });
+    this.getLead(id);
+  }
+
+  isButtonEdit = (status) => {
+    this.setState({
+      isEdit: status
+    });
+  }
+
+  formatDate = (date, format) => {
+    var d = new Date(date),
+      formattedDate;
+
+    switch (format) {
+      case 'y-m-d':
+        formattedDate = d.getFullYear() + '-' + ("0" + (d.getMonth() + 1)).slice(-2) + '-' + ("0" + d.getDate()).slice(-2);
+        break;
+      case 'd/m/y':
+        formattedDate = ("0" + d.getDate()).slice(-2) + '/' + ("0" + (d.getMonth() + 1)).slice(-2) + '/' + d.getFullYear();
+        break;
+      case 'm/d/y':
+        formattedDate = ("0" + (d.getMonth() + 1)).slice(-2) + '/' + ("0" + d.getDate()).slice(-2) + '/' + d.getFullYear();
+        break;
+      default:
+        formattedDate = d.getFullYear() + '-' + ("0" + (d.getMonth() + 1)).slice(-2) + '-' + ("0" + d.getDate()).slice(-2);
+    }
+
+    return formattedDate;
+
+  }
+
+  render() {
+    let {
+      leads,
+      lead,
+      isEdit
+    } = this.state;
+
+    return (
       <div>
-        <h1>All Leads</h1>
-        <AddLead getLeads={this.getLeads}/>
-        <ListLead leads={leads} deleteLead={this.deleteLead}/>
+        <h1> All Leads </h1>
+        <AddLead
+          getLeads={this.getLeads} lead={lead}
+          isEdit={isEdit} formatDate={this.formatDate}
+          isButtonEdit={this.isButtonEdit}
+        />
+        <br />
+        <ListLead
+          leads={leads} editLead={this.editLead}
+          deleteLead={this.deleteLead}
+          formatDate={this.formatDate}
+        />
       </div>
     )
   }
